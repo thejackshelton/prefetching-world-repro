97fe06d14baea203e373ffdc343149fbf53769c6
Dustin J Silk <dustinjsilk@gmail.com>
2024-06-25 11:57:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reproduce
+
+`$ pnpm i`
+
+`$ pnpm start`
+
+See 404 error in the browser when fetching the prefetch graph file.
---
+++ src/root.tsx
@@ -0,0 +1,33 @@
+import {
+  PrefetchGraph,
+  PrefetchServiceWorker,
+  component$,
+} from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
+
+import "./global.css";
+
+export default component$(() => {
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Don't remove the `<head>` and `<body>` elements.
+   */
+
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charset="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <RouterHead />
+      </head>
+      <body lang="en">
+        <RouterOutlet />
+        <PrefetchServiceWorker base={"/build/"} scope="/" />
+        <PrefetchGraph base={"/build/"} />
+      </body>
+    </QwikCityProvider>
+  );
+});
